f55e38731cab2214e47de3fc56288170ab421b06
Blady <81239615+Blady-DB@users.noreply.github.com>
2021-11-08 16:09:51 +0100

    Update Db.php

--- model/Db.php
+++ model/Db.php
@@ -1,18 +1,13 @@
 <?php
 
-// Cette class contient les données de connexion à la base de données et centralise toutes les méthodes qui seront utilisées partout
-
 class Db {
     // Affectation des variables pour la PDO (connexion à la base de données)
     private $servername = "localhost";
     private $dbname = "hospitale2n";
     private $username = "root";
     private $password = "";
-    public $conn;
-    public $result;
 
-    // Fonction permettant d'avoir la connexion à la BDD
-    protected function getConn() {
+    function __construct() {
         try {
             $conn = new PDO("mysql:host=$this->servername;dbname=$this->dbname;", $this->username, $this->password);
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -20,13 +15,16 @@ class Db {
             } catch(PDOException $e) {
             echo "Connection failed: " . $e->getMessage();
         }
+    }
+    protected function getConn() {
         return $this->conn;
     }
     
     protected function query(string $sql) {
         $stmt = $this->getConn()->prepare($sql);
         $stmt->execute();
-        $result = $stmt->fetchAll();
-        return $result;
+        return $stmt;
     }
+
+
 }
